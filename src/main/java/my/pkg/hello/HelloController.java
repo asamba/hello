ded2679d598b0ef81ba4bdf00e37dcc6e38c676e
asamba <anand.sambamoorthy@gmail.com>
2019-08-13 10:51:02 -0700

    Initial commit

---
+++ src/main/java/my/pkg/hello/HelloController.java
@@ -0,0 +1,17 @@
+package my.pkg.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by asamba on 7/27/19.
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping(path="/hello")
+    public String sayHello(){
+
+        return "Hello World!";
+    }
+}
